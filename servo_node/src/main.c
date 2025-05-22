0037895d601a06cb7af3bcb7ed1a30784bf6c270
DomRicho <DomRicho15@outlook.com>
2025-05-22 15:03:33 +1000

    servo stuff

--- servo_node/src/main.c
+++ servo_node/src/main.c
@@ -1,6 +1,84 @@
-#include <zephyr/kernel.h>
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-int main()
+/**
+ * @file Sample app to demonstrate PWM-based servomotor control
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/pwm.h>
+
+static const struct pwm_dt_spec tilt_servo = PWM_DT_SPEC_GET(DT_NODELABEL(tilt_servo));
+static const struct pwm_dt_spec pan_servo = PWM_DT_SPEC_GET(DT_NODELABEL(pan_servo));
+static const uint32_t min_pulse = DT_PROP(DT_NODELABEL(pan_servo), min_pulse);
+static const uint32_t max_pulse = DT_PROP(DT_NODELABEL(pan_servo), max_pulse);
+
+#define STEP PWM_USEC(100)
+
+enum direction {
+	DOWN,
+	UP,
+};
+
+int main(void)
 {
-    return 0;
+	uint32_t pulse_width = min_pulse;
+	enum direction dir = UP;
+	int ret;
+
+	printk("Servomotor control\n");
+
+	if (!pwm_is_ready_dt(&pan_servo)) {
+		printk("Error: PWM device %s is not ready\n", pan_servo.dev->name);
+		return 0;
+	}
+	if (!pwm_is_ready_dt(&tilt_servo)) {
+		printk("Error: PWM device %s is not ready\n", tilt_servo.dev->name);
+		return 0;
+	}
+    ret = pwm_set_cycles(pan_servo.dev, pan_servo.channel, pan_servo.period, PWM_MSEC(10), pan_servo.flags);
+    ret = pwm_set_cycles(tilt_servo.dev, tilt_servo.channel, tilt_servo.period, PWM_MSEC(10), tilt_servo.flags);
+
+	while (1) {
+		/*ret = pwm_set_pulse_dt(&servo, pulse_width);*/
+        printk("%s :: chl %d, T=%u, PW=%u\n", pan_servo.dev->name, pan_servo.channel, pan_servo.period, pulse_width);
+        ret = pwm_set_cycles(pan_servo.dev, pan_servo.channel, pan_servo.period, pulse_width, pan_servo.flags);
+		if (ret < 0) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return 0;
+		}
+        printk("%s :: chl %d, T=%u, PW=%u\n", tilt_servo.dev->name, tilt_servo.channel, tilt_servo.period, pulse_width);
+        ret = pwm_set_cycles(tilt_servo.dev, tilt_servo.channel, tilt_servo.period, pulse_width, tilt_servo.flags);
+		if (ret < 0) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return 0;
+		}
+
+		if (dir == DOWN) {
+			if (pulse_width <= min_pulse) {
+				dir = UP;
+				pulse_width = min_pulse;
+			} else {
+				pulse_width -= STEP;
+			}
+		} else {
+			pulse_width += STEP;
+
+			if (pulse_width >= max_pulse) {
+				dir = DOWN;
+				pulse_width = max_pulse;
+			}
+		}
+        printk("Pulse Width: %d\n", pulse_width);
+
+		/*k_sleep(K_SECONDS(1));*/
+        k_msleep(1000);
+	}
+	return 0;
 }
+
